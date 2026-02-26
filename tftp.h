951a78d2d40ffffc72cd2c6b13c85ea176871757
Dushyant <paitldp319@gmail.com>
2026-02-26 11:56:03 +0530

    Adding Neccessary header file

--- tftp.h
+++ tftp.h
@@ -1,14 +1,26 @@
 /* Common file for server & client*/
-
+/*  Name: Dushyant Patil
+    Project: Demonstration of Trivial File Transfer Protocol (TFTP)*/
 #ifndef TFTP_H
 #define TFTP_H
 
 #include <stdint.h>
 #include <arpa/inet.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define PORT 6969
 #define BUFFER_SIZE 516  // TFTP data packet size (512 bytes data + 4 bytes header)
 #define TIMEOUT_SEC 5    // Timeout in seconds
+#define IP_ADDRESS "127.0.0.1"
 
 // TFTP OpCodes
 typedef enum {
